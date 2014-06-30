4cef0a142faf091aa7052d779c2a0f8add2ef05e
laszloambrus <dr.ambrus.laszlo@gmail.com>
2014-06-30 14:33:26 +0200

    Symfony PS0 implemented, not fully functional yet

--- app/v1/index.php
+++ app/v1/index.php
@@ -7,6 +7,20 @@
     define('__SITE_ROOT__','/Task2/app/v1/');
 
     //Symphony classLoader
+    require_once __DIR__.'\vendors\Symfony\Component\ClassLoader\UniversalClassLoader.php';
+    use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+    $loader = new UniversalClassLoader();
+    $loader->registerNamespace('app\v1', __SERVER_ROOT__);
+
+    echo __SERVER_ROOT__;
+    $loader->register();
+
+    echo '<PRE>';
+    print_r($loader->getNamespaces());
+    echo '</PRE>';
+
+    require_once('api.php');
 
     //Fetching the URL
     $uri = $_SERVER['REQUEST_URI'];
@@ -17,21 +31,25 @@
     //Exploding URL
     $uri = explode('/',rtrim($uri,'/'));
 
-    //Preparing request
+    //Preparing request - I (think) i will move this part to my Abstract class
     //Concrete class for endpoint
     $request['endpoint'] = $uri[0];
     //Method in concrete class
     $request['method'] = $uri[1];
-    //Parameters
+    //Parameters - filters etc..
     $request['params'] = $uri[2];
 
     //get Origin for security reasons
     $origin = $_SERVER['HTTP_ORIGIN'];
-    echo $origin;
+    //echo $origin;
 
-    //Instantiate the API
+    //What's included - Just for testing purposes
+    echo '<PRE>';
+    print_r(get_included_files());
+    echo '</PRE>';
+
     try{
-       new api($request,$origin);
+        new \app\v1\api($request,$origin);
     }catch(Exception $e){
-       echo $e->getMessage();
+        echo $e->getMessage();
     }
