3400521c6ae36672caca9b599f4655c015881d2b
immi <dr.ambrus.laszlo@gmail.com>
2014-07-03 23:40:46 +0200

    Defining main task for the Model class. Working on data response function "getData".

--- app/v1/models/model.php
+++ app/v1/models/model.php
@@ -1,4 +1,23 @@
 <?PHP
+namespace v1\models;
 class Model{
+    //Data from concrete models
+    protected $datas = array();
 
+    public function __construct(){
+
+    }
+
+    //Get data and prepare it according to requested fileType
+    protected function getData($fileType){
+        switch ($fileType){
+            case 'json':
+                //Return data jason_encoded
+                echo json_encode($this->datas);
+                break;
+            default:
+                echo json_encode($this->datas);
+                break;
+        }
+    }
 }
